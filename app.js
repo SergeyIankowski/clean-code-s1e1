c1d0d595c2ac2dc783c915c507a2b15a6be748f7
SeregeyIankowski <garret933@gmail.com>
2022-10-30 15:04:17 +0300

    refactor: change classes and remove input id according BEM concept and rules 2.1, 1.1 html-and-css-extended.md

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-const taskInput = document.getElementById("new-task");// Add a new task.
+const taskInput = document.querySelector(".todo-app__input_new-task");// Add a new task.
 const addButton = document.getElementsByTagName("button")[0];// first button
 const incompleteTaskHolder = document.getElementById("incompleteTasks");// ul of #incompleteTasks
 const completedTasksHolder = document.getElementById("completed-tasks");// completed-tasks
